cf354d9733bbc22e09c7c909433a9ee2616ed632
DMQQ <kozakdamian73@gmail.com>
2024-03-05 18:30:22 +0100

    fix 2

--- app/[locale]/api/career/route.ts
+++ app/[locale]/api/career/route.ts
@@ -6,7 +6,68 @@ import * as zfd from "zod-form-data";
 import * as z from "zod";
 
 import { limiter } from "../config/limiter";
-import { postData, uploadFile } from "@/utils/ninox";
+
+type Tables =
+  | "collaboration"
+  | "JobOffers"
+  | "OfferApplication"
+  | "services"
+  | "Career";
+
+async function postData<T>(
+  data: T,
+  destination: {
+    table: Tables;
+  }
+) {
+  console.log("where log");
+
+  const res = await axios.post(
+    process.env.BASE_API_URL + `/${destination.table}/records`,
+    [
+      {
+        fields: data,
+      },
+    ],
+    {
+      headers: {
+        Authorization: "Bearer " + process.env.AUTH_TOKEN,
+      },
+    }
+  );
+
+  const recordId = res.data[0].id as number;
+
+  return recordId;
+}
+
+async function uploadFile(
+  file: Blob,
+  destination: {
+    table: Tables;
+    recordId: string | number;
+  }
+): Promise<boolean> {
+  const formdata = new FormData();
+  formdata.append("file", file);
+
+  try {
+    const res = await axios.post(
+      process.env.BASE_API_URL +
+        `/${destination.table}/records/${destination.recordId}/files`,
+      formdata,
+      {
+        headers: {
+          Authorization: "Bearer " + process.env.AUTH_TOKEN,
+        },
+      }
+    );
+
+    return res.status === 201 || res.status === 200;
+  } catch (error) {
+    throw new Error("Image not uploaded");
+  }
+}
 
 const careerSchema = zfd.formData({
   name: zfd.text(z.string().max(100)),
--- utils/ninox.ts
+++ utils/ninox.ts
@@ -1,61 +1,63 @@
-import axios from "axios";
+// import axios from "axios";
 
-type Tables =
-  | "collaboration"
-  | "JobOffers"
-  | "OfferApplication"
-  | "services"
-  | "Career";
+// type Tables =
+//   | "collaboration"
+//   | "JobOffers"
+//   | "OfferApplication"
+//   | "services"
+//   | "Career";
 
-export async function uploadFile(
-  file: Blob,
-  destination: {
-    table: Tables;
-    recordId: string | number;
-  }
-): Promise<boolean> {
-  const formdata = new FormData();
-  formdata.append("file", file);
+// export async function uploadFile(
+//   file: Blob,
+//   destination: {
+//     table: Tables;
+//     recordId: string | number;
+//   }
+// ): Promise<boolean> {
+//   const formdata = new FormData();
+//   formdata.append("file", file);
 
-  try {
-    const res = await axios.post(
-      process.env.BASE_API_URL +
-        `/${destination.table}/records/${destination.recordId}/files`,
-      formdata,
-      {
-        headers: {
-          Authorization: "Bearer " + process.env.AUTH_TOKEN,
-        },
-      }
-    );
+//   try {
+//     const res = await axios.post(
+//       process.env.BASE_API_URL +
+//         `/${destination.table}/records/${destination.recordId}/files`,
+//       formdata,
+//       {
+//         headers: {
+//           Authorization: "Bearer " + process.env.AUTH_TOKEN,
+//         },
+//       }
+//     );
 
-    return res.status === 201 || res.status === 200;
-  } catch (error) {
-    throw new Error("Image not uploaded");
-  }
-}
+//     return res.status === 201 || res.status === 200;
+//   } catch (error) {
+//     throw new Error("Image not uploaded");
+//   }
+// }
 
-export async function postData<T>(
-  data: T,
-  destination: {
-    table: Tables;
-  }
-) {
-  const res = await axios.post(
-    process.env.BASE_API_URL + `/${destination.table}/records`,
-    [
-      {
-        fields: data,
-      },
-    ],
-    {
-      headers: {
-        Authorization: "Bearer " + process.env.AUTH_TOKEN,
-      },
-    }
-  );
+// export async function postData<T>(
+//   data: T,
+//   destination: {
+//     table: Tables;
+//   }
+// ) {
+//   console.log("where log");
 
-  const recordId = res.data[0].id as number;
+//   const res = await axios.post(
+//     process.env.BASE_API_URL + `/${destination.table}/records`,
+//     [
+//       {
+//         fields: data,
+//       },
+//     ],
+//     {
+//       headers: {
+//         Authorization: "Bearer " + process.env.AUTH_TOKEN,
+//       },
+//     }
+//   );
 
-  return recordId;
-}
+//   const recordId = res.data[0].id as number;
+
+//   return recordId;
+// }
