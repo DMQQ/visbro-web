08261ccd09c56c457913d4e13224954e9c34c141
DMQQ <kozakdamian73@gmail.com>
2024-03-06 16:40:14 +0100

    vercel config?

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n.ts");
-export default withNextIntl({});
+export default withNextIntl({
+  env: {
+    AUTH_TOKEN: process.env.AUTH_TOKEN,
+    BASE_API_URL: process.env.BASE_API_URL,
+    DATABASE_ID: process.env.DATABASE_ID,
+    TEAM_ID: process.env.TEAM_ID,
+  },
+});
