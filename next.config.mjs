aca857e7b17d4efe38ed3a804694b51bff80f05b
DMQQ <kozakdamian73@gmail.com>
2024-03-06 16:42:16 +0100

    removed envs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n.ts");
-export default withNextIntl({
-  env: {
-    AUTH_TOKEN: process.env.AUTH_TOKEN,
-    BASE_API_URL: process.env.BASE_API_URL,
-    DATABASE_ID: process.env.DATABASE_ID,
-    TEAM_ID: process.env.TEAM_ID,
-  },
-});
+export default withNextIntl({});
