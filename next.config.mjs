efaa731e16a19ad9cfa46195a5a37f0a780e3442
DMQQ <kozakdamian73@gmail.com>
2024-05-30 01:21:07 +0200

    changed domains

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,22 @@ export default {
   ...withNextIntl({}),
 
   images: {
-    domains: ["localhost", "/api/images", "https://visbro-web.vercel.app"],
+    domains: [
+      "localhost",
+      "/api/images",
+      "https://visbro-web.vercel.app",
+      "https://visbro-web-dmqqs-projects.vercel.app",
+      "https://visbro-web.vercel.app/pl",
+    ],
+
+    remotePatterns: [
+      {
+        protocol: "https", // Specify the protocol (http or https)
+        hostname: "visbro-web.vercel.app", // Specify the hostname of your Vercel app
+        port: "", // Specify the port if needed, leave empty if not using a custom port
+        pathname: "/pl/api/images/**", // Specify the pathname pattern to match your route
+      },
+    ],
   },
 
   async rewrites() {
