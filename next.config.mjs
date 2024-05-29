ab0371cf4f0810b9ee26aa1ac582e71de0713fee
DMQQ <kozakdamian73@gmail.com>
2024-05-30 01:05:16 +0200

    FIX? set domains from which next should get images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n.ts");
-export default withNextIntl({});
+export default {
+  ...withNextIntl({}),
+
+  images: {
+    domains: [
+      "./public/images",
+      "/api/images",
+      "https://visbro-web.vercel.app/pl/api/images",
+    ],
+  },
+};
